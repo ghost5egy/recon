8db3586661d87fda292aa202572ce1f78ee69f4f
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2022-07-26 04:07:21 +0200

    Create recon-ghost.py

---
+++ recon-ghost.py
@@ -0,0 +1,19 @@
+import requests,json,socket
+domain = "ideasqr.com"
+resp = requests.get('https://crt.sh/?q={}&output=json'.format(domain))
+subdomains = []
+for sub in resp.json():
+        if sub['common_name'] in subdomains:
+                continue
+        subdomains.append(sub['common_name'])
+with open(domain + "-subdomains", 'w+') as f:
+        json.dump(subdomains, f)
+domainips = dict()
+for subdomain in subdomains:
+        try:
+                domainip = socket.gethostbyname(subdomain)
+                domainips[subdomain] = domainip
+        except:
+                continue
+with open(domain + "-subdomains-ips", 'w+') as f:
+        json.dump(domainips, f)
